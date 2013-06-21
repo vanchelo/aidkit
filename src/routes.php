63acba78defdabcf061876db903747a30a5918c2
Roman Kuba <mail@codebryo.com>
2013-06-21 18:34:03 +0200

    Updated Admin Routes to use own routes file

--- src/routes.php
+++ src/routes.php
@@ -2,12 +2,6 @@
 
 Route::group(array('prefix'=>Config::get('aidkit::config.prefix')),function(){
 
-	Route::get('/',function(){ return Redirect::route('dashboard'); });
-
-	//Handle login stuff
-	Route::controller('login','AidkitAuthController');
-
-	Route::get('logout','AidkitAuthController@logout');
-
+	include app_path().'/routes_aidkit.php';
 });
 
