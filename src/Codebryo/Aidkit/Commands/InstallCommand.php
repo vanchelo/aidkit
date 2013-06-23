d2f6ec91681f6b2878ac890fc3928fa30212cdf7
Roman Kuba <mail@codebryo.com>
2013-06-23 14:42:06 +0200

    Controller generation during Installation

--- src/Codebryo/Aidkit/Commands/InstallCommand.php
+++ src/Codebryo/Aidkit/Commands/InstallCommand.php
@@ -40,13 +40,19 @@ class InstallCommand extends Command {
      */
     public function fire()
     {
-    	if(File::exists(app_path().'/views_admin'))
-    		return $this->error('Aidkit seems to be installed allready!');
+        if(File::exists(app_path().'/views_admin'))
+            return $this->error('Aidkit seems to be installed allready!');
 
-    	$this->createFolders();
-    	$this->createViews();
-    	$this->createRoutes();
+        $this->createFolders();
+            $this->info('Basic Folder Structure has been created');
+        $this->createViews();
+            $this->info('Basic Views have been created');
+        $this->createControllers();
+            $this->info('Basic Controllers have been created');
+        $this->createRoutes();
+            $this->info('New Administrative Routes have been created');
         $this->createConfig();
+            $this->info('Configuration File has been published');
 
         // Call some other Functions
         $this->call('asset:publish',array('codebryo/aidkit'));
@@ -56,26 +62,22 @@ class InstallCommand extends Command {
 
     protected function createFolders()
     {
-    	// Create the admin_views Folder
-		File::makeDirectory(app_path().'/views_admin');
-		File::makeDirectory(app_path().'/views_admin/layout');
-		File::makeDirectory(app_path().'/views_admin/layout/partials');
+        // Create the admin_views Folder
+        File::makeDirectory(app_path().'/views_admin');
+        File::makeDirectory(app_path().'/views_admin/layout');
+        File::makeDirectory(app_path().'/views_admin/layout/partials');
         File::makeDirectory(app_path().'/views_admin/users');
 
-		$this->info('/views_admin Folder created');
-
-		File::makeDirectory(app_path().'/controllers/Admin');
-
-		$this->info('/controllers/Admin Folder created');
+        File::makeDirectory(app_path().'/controllers/Admin');
     }
 
     protected function createViews()
     {
-    	$templatePath = static::$templatePath;
-    	File::put(app_path().'/views_admin/layout/layout.blade.php',File::get($templatePath.'/views/layout.txt'));
-    	File::put(app_path().'/views_admin/layout/login.blade.php',File::get($templatePath.'/views/login.txt'));
-    	File::put(app_path().'/views_admin/layout/partials/navigation.blade.php',File::get($templatePath.'/views/partials/navigation.txt'));
-    	File::put(app_path().'/views_admin/layout/partials/profile.blade.php',File::get($templatePath.'/views/partials/profile.txt'));
+        $templatePath = static::$templatePath;
+        File::put(app_path().'/views_admin/layout/layout.blade.php',File::get($templatePath.'/views/layout.txt'));
+        File::put(app_path().'/views_admin/layout/login.blade.php',File::get($templatePath.'/views/login.txt'));
+        File::put(app_path().'/views_admin/layout/partials/navigation.blade.php',File::get($templatePath.'/views/partials/navigation.txt'));
+        File::put(app_path().'/views_admin/layout/partials/profile.blade.php',File::get($templatePath.'/views/partials/profile.txt'));
 
         // Create the User Views
 
@@ -83,20 +85,15 @@ class InstallCommand extends Command {
         File::put(app_path().'/views_admin/users/show.blade.php',File::get($templatePath.'/views/users/show.txt'));
         File::put(app_path().'/views_admin/users/edit.blade.php',File::get($templatePath.'/views/users/edit.txt'));
         File::put(app_path().'/views_admin/users/create.blade.php',File::get($templatePath.'/views/users/create.txt'));
-
-    	$this->info('Basic Views created');
-
     }
 
     protected function createRoutes()
     {
-    	$templatePath = static::$templatePath;
+        $templatePath = static::$templatePath;
         File::append(app_path().'/routes.php',File::get($templatePath.'/routes_append.txt'));
-        $this->info('/routes.php modified');
 
-    	File::put(app_path().'/routes_aidkit.php',File::get($templatePath.'/routes.txt'));
+        File::put(app_path().'/routes_aidkit.php',File::get($templatePath.'/routes.txt'));
 
-    	$this->info('/routes_aidkit created');
     }
 
     protected function createConfig()
@@ -105,5 +102,13 @@ class InstallCommand extends Command {
         File::makeDirectory(app_path().'/config/packages/codebryo');
         File::makeDirectory(app_path().'/config/packages/codebryo/aidkit');
         File::put(app_path().'/config/packages/codebryo/aidkit/config.php',File::get($templatePath.'/config.txt'));
+    }
+
+    protcted function createControllers()
+    {
+        $templatePath = static::$templatePath;
+        File::put(app_path().'/controllers/Admin/AuthController.php',File::get($templatePath.'/controllers/AuthController.txt'));
+        File::put(app_path().'/controllers/Admin/HomeController.php',File::get($templatePath.'/controllers/HomeController.txt'));
+        File::put(app_path().'/controllers/Admin/UserController.php',File::get($templatePath.'/controllers/UserController.txt'));
     }
 }
