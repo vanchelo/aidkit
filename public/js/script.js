cf93b20a431bcfb5f728debf8f73be6ef96690e2
Roman Kuba <mail@codebryo.com>
2013-07-13 18:05:02 +0200

    Updated Assets

--- public/js/script.js
+++ public/js/script.js
@@ -2,12 +2,36 @@
 
 	init();
 
+	var BASE_URL = $('base').attr('href'),
+		TEMPLATE_URL = BASE_URL + '/js/';
+
+	var HTMLcover = $('<div>',{class: 'js-cover'}),
+		HTMLalert = $('<div>',{class: 'js-alert'});
+
 	/* EVENTS */
 
 	$(window).resize(function() {
 		console.log('now');
 	  	fitContainers();
 	});
+
+	$('body').delegate('a.js-remove','click',function(evt){
+		evt.preventDefault();
+		evt.stopPropagation();
+
+		$('body').append(HTMLcover);
+		$('#wrapper').addClass('blur');
+
+		includeHTMLcontent('delete');
+
+	});
+
+	$('body').delegate('.js-cover','click',function(evt){
+		$('.js-cover, .js-alert').remove();
+		$('#wrapper').removeClass('blur');
+	});
+
+
 
 	/* FUNCTIONS */
 
@@ -23,6 +47,14 @@
 
 		$contentwrapper.css('min-height',minHeight);
 		$aside.height(minHeight);
+	}
+
+	function includeHTMLcontent(name){
+
+		$.get(TEMPLATE_URL + name, function(data) {
+			$('body').append(HTMLalert);
+		  	$('.js-alert').html(data);
+		});
 	}
 
 })(jQuery);
