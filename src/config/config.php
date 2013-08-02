5d5360cf27390940ba7fed996003ff902cfd9573
Roman Kuba <mail@codebryo.com>
2013-08-02 07:48:07 +0200

    Updated Missing Things and a first 404 page

--- src/config/config.php
+++ src/config/config.php
@@ -3,15 +3,15 @@
 return array(
 
 	// Give your Backend a Name
-	'title' 	=> 'Custom Admin Interface',
+	'title' 		=> 'Aidkit',
 
 	// What prefix shall be used for all the Generated urls
 	'urlprefix' 	=> 'admin',
 
 	// For constructing the Views a default layout file is provided
-	'layout'	=> 'layout.master',
+	'layout'		=> 'layout.master',
 
 	// There is also a login View provided
-	'login'		=> 'layout.login',
+	'login'			=> 'layout.login',
 	
 );
