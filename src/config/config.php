2f042a1f4d4ae9e9c2d86c3d0556d1e856fc56ee
Roman Kuba <mail@codebryo.com>
2013-08-10 15:44:40 +0200

    Define Adminroles from the config File

--- src/config/config.php
+++ src/config/config.php
@@ -13,5 +13,18 @@ return array(
 
 	// There is also a login View provided
 	'login'			=> 'layout.login',
+
+	// Feel free to add roles as you need throught the Application.
+	// i.e. Create a role that only has access to a specific area of the website
+	'roles'			=> array(
+		1	=> array(
+				'title' => 'superadmin',
+				'desc' 	=> 'Should be able to access any part of the Application'
+			),
+		2	=> array(
+				'title' => 'administrator',
+				'desc'	=> 'Should only be able to access certain parts of the Application'
+			)
+	)
 	
 );
