872fc55ba3017359348d6846c7ec4cd327fd2e0a
Roman Kuba <mail@codebryo.com>
2013-08-10 15:45:27 +0200

    users are now admins so it's clear they are only supposed to work on the backend

--- src/config/navigation.php
+++ src/config/navigation.php
@@ -8,14 +8,15 @@ return array(
 			'icon' 	=> 'icon-dashboard',
 			'url' => route('dashboard'),
 		),
-		'users' => array(
+		'admins' => array(
 			'icon' 	=> 'icon-user',
-			'url'	=> route('admin.users.index'),
+			'url'	=> route('admin.admins.index'),
+			'role'	=> 1,
 			
 			'sub-menu' => array(
 				'create' => array(
-					'title' => 'New User',
-					'url' 	=> route('admin.users.create')
+					'title' => 'New Admin',
+					'url' 	=> route('admin.admins.create')
 				)	
 			)
 			
